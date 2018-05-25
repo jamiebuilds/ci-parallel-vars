2bb05ed83bcb20d734c244dda0c86aae7dcdb5c8
Jamie Kyle <me@thejameskyle.com>
2018-05-25 13:05:47 -0700

    init commit

---
+++ test.js
@@ -0,0 +1,34 @@
+// @flow
+'use strict';
+const test = require('ava');
+const path = require('path');
+const spawn = require('spawndamnit');
+
+const FIXTURE = path.join(__dirname, 'fixture.js');
+
+async function run(env) {
+  let res = await spawn('node', [FIXTURE], {
+    env: Object.assign({}, process.env, env),
+  });
+  return JSON.parse(res.stdout.toString());
+}
+
+test('nothin', async t => {
+  t.is(await run({}), null);
+});
+
+test('partial', async t => {
+  t.is(await run({
+    CI_NODE_INDEX: 0
+  }), null);
+});
+
+test('both', async t => {
+  t.deepEqual(await run({
+    CI_NODE_INDEX: 1,
+    CI_NODE_TOTAL: 3,
+  }), {
+    index: 1,
+    total: 3
+  });
+});
